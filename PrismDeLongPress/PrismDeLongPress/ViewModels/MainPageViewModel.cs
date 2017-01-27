7761702fec95e33a643d8eb115629eaa8233b548
Keisuke Ishikawa <ishikawa-k@mis-kyushu.jp>
2017-01-27 13:39:09 +0900

    listviewで確認、Androidはアウト

--- PrismDeLongPress/PrismDeLongPress/ViewModels/MainPageViewModel.cs
+++ PrismDeLongPress/PrismDeLongPress/ViewModels/MainPageViewModel.cs
@@ -9,6 +9,13 @@ using System.Windows.Input;
 
 namespace PrismDeLongPress.ViewModels
 {
+    public class TListItem
+    {
+        public string Title { get; set; }
+        public string Detail { get; set; }
+    }
+
+
     public class MainPageViewModel : BindableBase
     {
         private IPageDialogService _pageDialogService;
@@ -16,14 +23,24 @@ namespace PrismDeLongPress.ViewModels
         public ICommand LabelPressCommand { get; }
         public ICommand EntryPressCommand { get; }
         public ICommand TablePressCommand { get; }
+        public ICommand ListPressCommand { get; }
+
+        public List<TListItem> ListItems { get; }
 
         public MainPageViewModel(IPageDialogService PageDialogService)
         {
             _pageDialogService = PageDialogService;
 
-            LabelPressCommand = new DelegateCommand(async () => { await _pageDialogService.DisplayAlertAsync("Label", "Long Pressed", "OK"); } );
+            LabelPressCommand = new DelegateCommand(async () => { await _pageDialogService.DisplayAlertAsync("Label", "Long Pressed", "OK"); });
             EntryPressCommand = new DelegateCommand(async () => { await _pageDialogService.DisplayAlertAsync("Entry", "Long Pressed", "OK"); });
             TablePressCommand = new DelegateCommand(async () => { await _pageDialogService.DisplayAlertAsync("Table", "Long Pressed", "OK"); });
+            ListPressCommand = new DelegateCommand(async () => { await _pageDialogService.DisplayAlertAsync("List", "Long Pressed", "OK"); });
+
+            ListItems = new List<TListItem>();
+            for (int i = 0; i < 100; i++)
+            {
+                ListItems.Add(new TListItem() { Title = $"title-{i}", Detail = $"detail-{i}" });
+            }
         }
     }
 }
